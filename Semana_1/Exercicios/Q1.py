2bd56b8b1c10a70543126a019016d2943879f96c
Daniel Lopes <ddanielssoares@gmail.com>
2020-09-21 15:01:52 -0300

    Semana 1
    Exercício 1, 2.

---
+++ Semana_1/Exercicios/Q1.py
@@ -0,0 +1,12 @@
+# Exercicio 1
+'''
+Escreva intruções para converter a temperatura de "C" graus Celsius 
+para Fahrenheit sabendo que temos que multiplicar C por 9/5 e depois somar 32.  
+O resultado deve ser armazenado na variável "F".
+Imprima o resultado da seguinte forma "xx graus Celsius corresponde a yy graus Fahrenheit".
+Note que a sequência está iniciada com C = 65.
+Assim, o resultado a ser exibido deve ser "65 graus Celsius corresponde a 149.0 graus Fahrenheit".
+'''
+C = 65
+F = C * 9/5 + 32
+print(C,'graus Celsius corresponde a',F,'graus Fahrenheit')
---
+++ Semana_1/Exercicios/Q2.py
@@ -0,0 +1,18 @@
+# Exercício 2
+'''
+Escreva instruções para converter a temperatura de "C" graus Celsius para Fahrenheit 
+sabendo que temos que multiplicar C por 9/5 e depois somar 32.  
+O resultado deve ser armazenado na variável "F".
+Imprima o resultado da seguinte forma "?? graus Celsius corresponde a ?? graus Fahrenheit".
+O programa deve imprimir respostas para duas temperaturas:
+C = 55  e  C = 65
+Assim, o resultado a ser exibido deve ser:
+"55 graus Celsius corresponde a 131.0 graus Fahrenheit".
+"65 graus Celsius corresponde a 149.0 graus Fahrenheit".
+'''
+C = 55
+F = C * 9/5 + 32
+print(C,'graus Celsius corresponde a',F,'graus Fahrenheit')
+C = 65
+F = C * 9/5 + 32
+print(C,'graus Celsius corresponde a',F,'graus Fahrenheit')
